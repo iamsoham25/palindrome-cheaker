962e8e6d390e7fd2e09e37141ec97b899b022a25
iamsoham25 <sohamthoke@gmail.com>
2024-12-17 19:05:52 +0530

    Add files via upload

---
+++ palindrome cheaker/script.js
@@ -0,0 +1,25 @@
+const txtInput = document.querySelector(".inputs input"),
+      checkBtn = document.querySelector(".inputs button"),
+      infoTxt = document.querySelector(".info-txt");
+
+let filterInput;
+
+checkBtn.addEventListener("click", () => {
+    let reverseInput = filterInput.split("").reverse().join("");
+    infoTxt.style.display = "block";
+    if (filterInput !== reverseInput) {
+        infoTxt.innerHTML = `No, <span>'${txtInput.value}'</span> isn't a palindrome!`;
+    } else {
+        infoTxt.innerHTML = `Yes, <span>'${txtInput.value}'</span> is a palindrome!`;
+    }
+});
+
+txtInput.addEventListener("keyup", () => {
+    filterInput = txtInput.value.toLowerCase().replace(/[^A-Z0-9]/ig, "");
+    if (filterInput) {
+        checkBtn.classList.add("active");
+    } else {
+        infoTxt.style.display = "none";
+        checkBtn.classList.remove("active");
+    }
+});
